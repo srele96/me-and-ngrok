3c572184c3142f931c6478801e83e65a4ab74935
s.kostic <s.kostic@netconomy.net>
2025-03-13 00:55:50 +0100

    add react

---
+++ client/README.md
@@ -0,0 +1,12 @@
+# Client
+
+Use dist directory so files are used by service in docker watch mode.
+
+## Docs
+
+Run the build watcher:
+
+```
+node ./esbuilder.js
+```
+
---
+++ client/esbuilder.js
@@ -0,0 +1,63 @@
+const esbuild = require('esbuild');
+
+const rebuildPlugin = {
+  name: 'rebuild-plugin',
+  setup(build) {
+    build.onStart(() => {
+      console.log('Build started...');
+    });
+
+    build.onEnd(result => {
+      if (result.errors.length === 0) {
+        console.log('Build completed successfully.');
+      }
+    });
+  }
+};
+
+async function esbuilder() {
+  const ctx = await esbuild.context({
+    entryPoints: ['src/index.js'],
+    bundle: true,
+    outfile: 'dist/bundle.js',
+    format: 'esm',
+    sourcemap: true,
+    minify: false,
+    plugins: [rebuildPlugin], // Use the rebuild plugin here
+    loader: {
+      '.js': 'jsx',
+      '.jsx': 'jsx',
+    },
+    jsx: 'transform',
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
+  });
+
+  await ctx.watch();
+
+  console.log('Esbuilder watching...');
+
+  // Function to dispose of context on exit
+  function exitGracefully() {
+    console.log('Disposing of context.');
+
+    ctx.dispose()
+      .then(() => {
+        console.log('Disposed of context successfully.');
+        process.exit(0);
+      })
+      .catch((error) => {
+        console.log('Error occurred while disposing of context: ' + error.message);
+        process.exit(1);
+      });
+  }
+
+  // Handle SIGINT and SIGTERM gracefully
+  process.on('SIGINT', exitGracefully);
+  process.on('SIGTERM', exitGracefully);
+}
+
+// Start the esbuild process
+esbuilder().catch(err => {
+  console.error('Error occurred: ' + err.message);
+});
